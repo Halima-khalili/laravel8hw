8cc48eda6832a3989bc54dfa149344be1caf558d
halima <halima.khalili77@gmail.com>
2021-03-28 19:27:58 +0430

    Registering and Validating form

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserAuthController extends Controller
 {
@@ -14,6 +15,23 @@ class UserAuthController extends Controller
         return view('admin.register');
     }
     function create(Request $request){
-        return $request->input();
+        //Validate Requests
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:5|max:12'
+        ]);
+        //Registering the User
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $query = $user->save();
+        
+        if($query){
+            return back()->with('success','You have been successfully register');
+        }else{
+            return bakc()->with('fail', 'Somthing went wrong');
+        }
     }
 }
--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -10,17 +10,34 @@
                 @csrf
                     <h3 class="text-center">Register yourself</h3>
                     <hr>
+                
+                @if (Session::get('success'))
+                    <div class="results">
+                        <div class="alert alert-success">
+                            {{ Session::get('success') }}
+                        </div>
+                    </div>
+                @elseif (Session::get('fail'))
+                    <div class="results">
+                        <div class="alert alert-danger">
+                            {{ Session::get('fail') }}
+                        </div>
+                    </div>
+                @endif
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" name="name" placeholder="Enter FullName" class="form-control">
+                    <input type="text" name="name" placeholder="Enter full name" class="form-control" value="{{ old('name') }}">
+                    <span class="text-danger">@error('name') {{ $message }} @enderror</span>
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="text" name="email" placeholder="Enter email" class="form-control">
+                    <input type="text" name="email" placeholder="Enter email" class="form-control" value="{{ old('email') }}">
+                    <span class="text-danger">@error('email') {{ $message }} @enderror</span>
                 </div>
                 <div class="form-group">
                     <label for="email">Password</label>
                     <input type="password" name="password" placeholder="Enter password" class="form-control">
+                    <span class="text-danger">@error('password') {{ $message }} @enderror</span>
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-block btn-primary">Register</button>
